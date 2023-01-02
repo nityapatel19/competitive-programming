70dcade8c6df7c8c26276bbdde6826b4bbc7723d
nityapatel19 <nityapatel19@gmail.com>
2023-01-02 16:13:00 +0530

    Added another solution

--- leetcode/word-pattern.cpp
+++ leetcode/word-pattern.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool wordPattern(string pattern, string s) {
+    // It is not slow.
+    /*bool wordPattern(string pattern, string s) {
         map<char, string> mp; // character to word
         set<string> visited; // visited words
         int j = 0; // pointer in s
@@ -46,6 +47,40 @@ public:
         if (j < s.length())
             return false;
 
+        return true;
+    }*/
+
+    // Just a different approach.
+    bool wordPattern(string pattern, string s) {
+        map<char, string> c2s;
+        map<string, char> s2c;
+
+        stringstream sentence(s);
+        string word;
+        vector<string> words;
+        while (sentence >> word)
+        {
+            words.push_back(word);
+        }
+
+        if (words.size() != pattern.length())
+        {
+            return false;
+        }
+
+        for (int i = 0; i < pattern.length(); i++)
+        {
+            c2s[pattern[i]] = words[i];
+            s2c[words[i]] = pattern[i];
+        }
+        
+        for (int i = 0; i < pattern.length(); i++)
+        {
+            if (c2s[pattern[i]] != words[i] || s2c[words[i]] != pattern[i])
+            {
+                return false;
+            }
+        }
         return true;
     }
 };
