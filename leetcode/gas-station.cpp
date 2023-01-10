fa88131913ce9921d32c19307f9bee5d709f5539
nityapatel19 <nityapatel19@gmail.com>
2023-01-10 10:10:56 +0530

    Optimized

--- leetcode/gas-station.cpp
+++ leetcode/gas-station.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int n = gas.size(), i = 0;
-        int diff[n] = {0};
-
-        for (int i = 0; i < n; ++i)
+    // I don't know why,
+    // but if total gas > total cost,
+    // a solution is always possible.
+    /*int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size(), i = 0, sum = 0;
+        vector<int> diff(n,0);
+        for (i = 0; i < n; ++i)
         {
             diff[i] = gas[i] - cost[i];
+            sum += diff[i];
         }
 
-        int max_yet = 0, curr_sum = 0, left = 0, right = 0;
-        for (int i = 0; i < n; ++i)
+        if (sum < 0) return -1;
+
+        int curr_sum = 0, left = 0;
+        for (i = 0; i < n; i++)
         {
             curr_sum += diff[i];
             if (curr_sum < 0)
@@ -18,14 +23,27 @@ public:
                 curr_sum = 0;
                 left = i+1;
             }
-            
-            if (curr_sum > max_yet)
+        }
+        return left;
+    }*/
+
+    // Little faster.
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int balance = 0;
+        int curr_gas = 0;
+        int starting_point = 0;
+
+        for (int i = 0; i < n; ++i)
+        {
+            curr_gas += gas[i]-cost[i];
+            balance += gas[i]-cost[i];
+            if (curr_gas < 0)
             {
-                right = i;
-                max_yet = curr_sum;
+                starting_point = i+1;
+                curr_gas = 0;
             }
         }
-
-        return left;
+        return (balance >= 0 ? starting_point : -1);
     }
 };
--- leetcode/max-points-on-a-line.cpp
+++ leetcode/max-points-on-a-line.cpp
@@ -1,17 +1,18 @@
 class Solution {
-public:
+// Slow due to different functions.
+/*private:
     double slope(vector<int> p1, vector<int> p2) {
-        double dx = p2[0]-p1[0];
-        double dy = p2[1]-p1[1];
+        int dx = p2[0]-p1[0];
+        int dy = p2[1]-p1[1];
 
         if (dx == 0)
         {
             return DBL_MAX;
         }
         
-        return dy/dx;
+        return dy/(double)dx;
     }
-
+public:
     int maxPoints(vector<vector<int>>& points) {
         int n = points.size(), max_points = 0;
         double m;
@@ -23,6 +24,44 @@ public:
                 if (i == j) continue;
 
                 m = slope(points[i], points[j]);
+                slope_freq[m]++;
+                max_points = max(max_points, slope_freq[m]);
+            }
+        }
+        return max_points+1;
+    }*/
+public:
+    // Still not the fastest.
+    /*int maxPoints(vector<vector<int>>& points) {
+        int n = points.size(), max_points = 0;
+        double m;
+        for (int i = 0; i < n; ++i)
+        {
+            map<double, int> slope_freq;
+            for (int j = 0; j < n; ++j)
+            {
+                if (i == j) continue;
+
+                m = (points[i][0] != points[j][0]) ? (points[j][1]-points[i][1])/(double)(points[j][0]-points[i][0]) : DBL_MAX;
+                slope_freq[m]++;
+                max_points = max(max_points, slope_freq[m]);
+            }
+        }
+        return max_points+1;
+    }*/
+
+    // Faster. Iterates less. Just checks overall which slopes are most common.
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size(), max_points = 0;
+        double m;
+        for (int i = 0; i < n; ++i)
+        {
+            map<double, int> slope_freq;
+            for (int j = i+1; j < n; ++j)
+            {
+                if (i == j) continue;
+
+                m = (points[i][0] != points[j][0]) ? (points[j][1]-points[i][1])/(double)(points[j][0]-points[i][0]) : DBL_MAX;
                 slope_freq[m]++;
                 max_points = max(max_points, slope_freq[m]);
             }
