e0ad1f3c93fc850b53625e554171dae363013e69
nityapatel19 <nityapatel19@gmail.com>
2023-02-04 10:41:20 +0530

    Optimized

--- leetcode/permutation-in-string.cpp
+++ leetcode/permutation-in-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
+    // Slow. Use hashmap instead of multiset.
+    /*bool checkInclusion(string s1, string s2) {
         int left = 0, right = s1.length();
         int n = s2.length();
         if (n < right) return false;
@@ -15,6 +16,38 @@ public:
             }
             window.erase(window.find(s2[left++]));
             window.insert(s2[right++]);
+        } while (right <= n);
+
+        return false;
+    }*/
+
+    // Faster. More efficient.
+    bool is_equal(int a[26], int b[26]) {
+        for (int i = 0; i < 26; ++i)
+            if (a[i] != b[i])
+                return false;
+        return true;
+    }
+    bool checkInclusion(string s1, string s2) {
+        int left = 0, right = s1.length();
+        int n = s2.length();
+        if (n < right) return false;
+
+        int letters[26] = {0};
+        int window[26] = {0};
+        for (int i = 0; i < right; i++)
+        {
+            letters[s1[i]-'a']++;
+            window[s2[i]-'a']++;
+        }
+        do
+        {
+            if (is_equal(letters, window))
+            {
+                return true;
+            }
+            window[s2[left++]-'a']--;
+            window[s2[right++]-'a']++;
         } while (right <= n);
 
         return false;
