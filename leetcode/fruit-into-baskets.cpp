75093bc259fe6aded71a64efeb9e581ab9f34d3b
nityapatel19 <nityapatel19@gmail.com>
2023-02-07 19:21:23 +0530

    07-02-2023

---
+++ leetcode/fruit-into-baskets.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    // O(n^2). Slow.
+    /*int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        int max_count = INT_MIN;
+        set<int> types;
+        for (int left = 0; left < n; ++left)
+        {
+            int right = left;
+            while (right < n)
+            {
+                if (types.find(fruits[right]) == types.end() && types.size() == 2)
+                    break;
+
+                types.insert(fruits[right]);
+                right++;
+
+            }
+            types.clear();
+            max_count = max(max_count, right-left);
+        }
+        return max_count;
+    }*/
+
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        unordered_map<int,int> basket;
+        int left = 0, right = 0;
+        int max_count = 0, curr_count = 0;
+
+        while (right < n)
+        {
+            basket[fruits[right]]++;
+            curr_count++;
+            while (basket.size() > 2)
+            {
+                basket[fruits[left]]--;
+                curr_count--;
+                if (basket[fruits[left]] == 0)
+                    basket.erase(fruits[left]);
+
+                left++;
+            }
+            max_count = max(max_count, curr_count);
+            right++;
+        }
+        return max_count;
+    }
+};
